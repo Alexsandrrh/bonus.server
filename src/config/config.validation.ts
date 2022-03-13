2e1d36b823437684eeb4c5a728d83689b374c82b
Aleksandr Sadov <sascha.sadov@yandex.ru>
2022-03-13 13:19:58 +0300

    update database uri

--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -2,6 +2,6 @@ import * as Joi from 'joi';
 
 export const ConfigValidation = Joi.object({
   PORT: Joi.string().default(3000),
-  DATABASE_URI: Joi.string().default('mongodb://localhost:27017/bonus-db'),
+  DATABASE_URI: Joi.string().default('mongodb://127.0.0.1:27017/bonus-db'),
   ACCESS_TOKEN_SECRET: Joi.string().default('secret_key'),
 });
